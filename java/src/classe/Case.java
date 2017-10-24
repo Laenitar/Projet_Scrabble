70ca3dfaf9508ac0a3456fd35bbc9400fd22f352
Elesye <32192225+Elesye@users.noreply.github.com>
2017-10-24 13:28:59 +0200

    Update Case.java

--- java/src/classe/Case.java
+++ java/src/classe/Case.java
@@ -10,5 +10,16 @@ public class Case implements Case_Itf{
 	Boolean LettreTriple;
 	
 	Piece piece = null;
+	ArrayList<String> motH = new ArrayList<String>();
+	
+		public Case(boolean motDouble, boolean motTriple, boolean lettreDouble, boolean lettreTriple, Piece piece) {
+		super();
+		this.motDouble = motDouble;
+		this.motTriple = motTriple;
+		this.lettreDouble = lettreDouble;
+		this.lettreTriple = lettreTriple;
+		this.piece = piece;
+	}
+
 		
 }
